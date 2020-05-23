6c2107e083611746e8aa58369fa10f0c63cac120
Nobuyuki Takahiro <studio.quad9@gmail.com>
2020-05-24 08:00:05 +0900

    for github peges, setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from './Header'
+import Aside from './Aside'
+import MDEdit from './MDEdit'
+import Footer from './Footer'
+import './assets/css/reset.css'
+import "easymde/dist/easymde.min.css"
+import './assets/css/style.css'
+
+const element = (
+  <div id="container">
+    <Header />
+    <Aside />
+    <MDEdit />
+    <Footer />
+  </div>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
