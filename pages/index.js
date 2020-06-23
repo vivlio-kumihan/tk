dcefc1233a221d3a717eb91d27f3e0aaf676518c
Nobuyuki Takahiro <studio.quad9@gmail.com>
2020-06-23 14:15:39 +0900

    Static Generation

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head'
 import Layout, { siteTilte } from "../components/layout"
 import utilStyles from "../styles/utils.module.css"
+import { getSortedPostsData } from "../lib/posts"
 
-const Home = () => {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: { allPostsData }
+  }
+}
+
+const Home = ({allPostsData}) => {
   return (
     <Layout home>
       <Head>
@@ -14,6 +22,22 @@ const Home = () => {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMD} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLG}>
+          Blog
+        </h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({id, date, title}) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
