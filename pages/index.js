daae7d73ff9c38b8d764f8105087ecb199909e0b
Nobuyuki Takahiro <studio.quad9@gmail.com>
2020-07-01 17:00:46 +0900

    dynamic routing

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Layout, { siteTilte } from "../components/layout"
 import utilStyles from "../styles/utils.module.css"
 import { getSortedPostsData } from "../lib/posts"
+import Link from "next/link"
+import Date from "../components/date"
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -29,14 +31,14 @@ const Home = ({allPostsData}) => {
         </h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({id, date, title, tag}) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
+            <li className = { utilStyles.listItem } key = { id } >
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
-              <br />
-              {tag}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
@@ -46,3 +48,41 @@ const Home = ({allPostsData}) => {
 }
 
 export default Home
+
+
+// const Home = ({allPostsData}) => {
+//   return (
+//     <Layout home>
+//       <Head>
+//         <title>{siteTilte}</title>
+//       </Head>
+//       <section className={utilStyles.headingMd}>
+//         <p>[Your Self Introduction]</p>
+//         <p>
+//           (This is a sample website - you’ll be building a site like this on{' '}
+//           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+//         </p>
+//       </section>
+//       <section className={`${utilStyles.headingMD} ${utilStyles.padding1px}`}>
+//         <h2 className={utilStyles.headingLG}>
+//           Blog
+//         </h2>
+//         <ul className={utilStyles.list}>
+//           {allPostsData.map(({id, date, title, tag}) => (
+//             <li className={utilStyles.listItem} key={id}>
+//               {title}
+//               <br />
+//               {id}
+//               <br />
+//               {date}
+//               <br />
+//               {tag}
+//             </li>
+//           ))}
+//         </ul>
+//       </section>
+//     </Layout>
+//   )
+// }
+
+// export default Home
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,22 +1,33 @@
 import Layout from "../../components/Layout"
+import Date from "../../components/date"
 import { getAllPostIds, getPostData } from "../../lib/posts"
+import Head from "next/head"
+import utilStyles from "../../styles/utils.module.css"
 
 // 投稿をレンダリングする
 export default function Post({ postData }) {
   return (
     <Layout>
+    <Head>
+      <title>{postData.tilte}</title>
+    </Head>
+    <article>
+      <h1 className={utilStyles.headingXL}>
       {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      {postData.tag}
-      <br />
+      </h1>
+      <div className={utilStyles.lightText} >
+        {/* Replace {postData.date} with this */}
+        <Date dateString={postData.date} />
+      </div>
+      {/* postコンポーネントを更新して、dangerouslySetInnerHTMLを使いcontentHtmlをレンダリングする。 */}
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </article>
     </Layout>
   )
 }
+
+// {postData.id}
+// {postData.tag}
 
 // id = ファイル名のリストを返す非同期関数を定義
 export async function getStaticPaths() {
@@ -28,6 +39,7 @@ export async function getStaticPaths() {
 
 // 与えられたid = ページ名を持つブログ記事に必要なデータを取得する非同期関数を定義
 export async function getStaticProps({ params }) {
+  // 関連してここにもawaitキーワードが必要。
   const postData = await getPostData(params.id)
   return {
     props: {
