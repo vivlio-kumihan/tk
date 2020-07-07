df04b508b875e8029eb7bf564d4572271409b855
Nobuyuki Takahiro <studio.quad9@gmail.com>
2020-07-07 15:50:26 +0900

    一旦githubにdeploy用で作った最終形まで戻す。

--- components/date.js
+++ components/date.js
@@ -3,7 +3,7 @@ import { parseISO, format } from "date-fns"
 export default function Date({ dateString }) {
   const date = parseISO(dateString)
   return (
-    <time dataTime={dateString}>
+    <time datatime={dateString}>
       {format(date, "LLLL d, yyyy")}
     </time>
   )
