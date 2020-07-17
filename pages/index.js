4c992cd967220a6080aba15500c6872a46d60a69
Nobuyuki Takahiro <studio.quad9@gmail.com>
2020-07-17 10:43:11 +0900

    for index photo layout

--- pages/index.js
+++ pages/index.js
@@ -21,15 +21,13 @@ export default function Home() {
       <div className={indexStyle.ourWork}>
         <h2 className={utilStyle.headingLg}><span>Our Wroks</span></h2>
         <div className={indexStyle.ourWorkImgs}>
-          <div><img src="/images/photo01.jpg" alt="" /></div>
-          <div><img src="/images/photo02.jpg" alt="" /></div>
-          <div><img src="/images/photo03.jpg" alt="" /></div>
-          <div><img src="/images/photo04.jpg" alt="" /></div>
-          <div><img src="/images/photo05.jpg" alt="" /></div>
-          <div><img src="/images/photo06.jpg" alt="" /></div>
-          <div><img src="/images/photo07.jpg" alt="" /></div>
-          <div><img src="/images/photo08.jpg" alt="" /></div>
-          <div><img src="/images/photo09.jpg" alt="" /></div>
+          <div><img src="/images/img_obamj_covers1.jpg" alt="" /></div>
+          <div><img src="/images/img_obamj_covers2.jpg" alt="" /></div>
+          <div><img src="/images/img_nishipri_brochure_cover2.jpg" alt="" /></div>
+          <div><img src="/images/img_nishipri_brochure_cover3.jpg" alt="" /></div>
+          <div><img src="/images/img_book_cover1.jpg" alt="" /></div>
+          <div><img src="/images/img_nishipri_brochure_cover1.jpg" alt="" /></div>
+          <div><img src="/images/img_books.jpg" alt="" /></div>
         </div>
       </div>      
     </Layout>
