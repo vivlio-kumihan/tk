70d4c29750f8360352437323490b52d625b27146
Nobuyuki Takahiro <studio.quad9@gmail.com>
2020-07-27 17:19:36 +0900

    study move instance

--- pages/app.js
+++ pages/app.js
@@ -1,15 +1,12 @@
 import Layout, { siteTitle } from "../components/layout"
-import {greetHello} from "../lib/util"
+import Util from "../lib/util"
 
 
-export default function myApp() {
-  const stone = "石"
+export default function App() {
   return (
     <Layout>
-      {greetHello}
-      <greetHello />
-      <h1>直に書く</h1>
-      <h2>{stone}</h2>
+      <h1>Hello</h1>
+      <Util />
     </Layout>
   )
 }
