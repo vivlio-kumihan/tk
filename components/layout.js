2f96c5251da0b6ab6d68a48e73765030c6bb4deb
Nobuyuki Takahiro <studio.quad9@gmail.com>
2021-04-07 05:31:01 +0900

    add md

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Nobuyuki Takahiro'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'vivlio kumihan Website'
 
 export default function Layout ({ children, home }) {
   return (
