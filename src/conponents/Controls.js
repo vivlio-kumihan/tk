c06df08a5d40a29040fef0de28bfba6fbaa1ac1b
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-09-08 06:05:40 +0900

    setting volume

--- src/conponents/Controls.js
+++ src/conponents/Controls.js
@@ -1,5 +1,17 @@
 import { useState, useEffect, useRef, useCallback } from "react";
-import { BsSkipBackward } from "react-icons/bs";
+
+// どこにも参照されていないからコメントアウトする。
+// import { BsSkipBackward } from "react-icons/bs";
+
+// 参照されているが半濁になる？　
+// しかもコメントアウトしても曲のインスタンスがある。　
+// なぜ？　webpack compiled successfully　となるのか？
+// 親でインポートしてるからか？
+// でも、コメントアウトしたらvolumeが効いたので良しとする。
+// import { tracks } from "../data/tracks";
+
+// volume
+// import {IoMdVolumeHigh, IoMdVolumeOff, IoMdVolumeLow } from "react-icons/io"
 
 // icons
 import {
@@ -10,14 +22,18 @@ import {
   IoPlaySharp,
   IoPauseSharp
 } from 'react-icons/io5';
-import { tracks } from "../data/tracks";
+
 
 const Controls = ({ audioRef, progressBarRef, duration, setTimeProgress, tracks, trackIndex, setTrackIndex, setCurrentTrack }) => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(60);
+  const playAnimationRef = useRef();
+  
+
   const togglePlayPause = () => {
     setIsPlaying((prev) => !prev);
   };
-  const playAnimationRef = useRef();
+
   const repeat = useCallback(() => {
     // console.log('run');
     const currentTime = audioRef.current.currentTime;
@@ -44,14 +60,22 @@ const Controls = ({ audioRef, progressBarRef, duration, setTimeProgress, tracks,
     // useCallback()関数が再生を開始したらずっと動き続けていることに違和感がある。
     // とりあえず従うか。
     playAnimationRef.current = requestAnimationFrame(repeat);
-  }, [isPlaying, audioRef, repeat]);
+  }, [volume, isPlaying, audioRef, repeat]);
+
+  useEffect(() => {
+    if (audioRef) {
+      audioRef.current.volume = volume / 100;
+    }
+  }, [volume, audioRef]);
 
   const skipForward = () => {
     audioRef.current.currentTime += 15;
   };
+
   const skipBackward = () => {
     audioRef.current.currentTime -= 15;
   };
+
   const handlePrevious = () => {
     if (trackIndex === 0) {
       let lastTrackIndex = tracks.length -1;
@@ -62,6 +86,7 @@ const Controls = ({ audioRef, progressBarRef, duration, setTimeProgress, tracks,
       setCurrentTrack(tracks[trackIndex - 1]);
     }
   };
+
   const hancleNext = () => {
     if (trackIndex >= tracks.length - 1) {
       setTrackIndex(0);
@@ -71,6 +96,7 @@ const Controls = ({ audioRef, progressBarRef, duration, setTimeProgress, tracks,
       setCurrentTrack(tracks[trackIndex + 1]);
     }
   };
+
   return (
     <div className="controls-wrapper">
       <div className="controls">
@@ -89,6 +115,21 @@ const Controls = ({ audioRef, progressBarRef, duration, setTimeProgress, tracks,
         <button onClick={hancleNext}>
           <IoPlaySkipForwardSharp />
         </button>
+      </div>
+      <div className="volume">
+        <div className="volume">
+          <button>icons</button>
+          <input
+            type="range"
+            min={0}
+            max={100}
+            value={volume}
+            onChange={(e) => setVolume(e.target.value)} 
+            style={{
+              background: `linear-gradient(to right, #f50 ${volume}%, #ccc ${volume}%)`,
+            }}
+          />
+        </div>
       </div>
     </div>
   );
