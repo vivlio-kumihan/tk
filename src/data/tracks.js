8df6af78ca83110258b06f9ae0c00fa91ddbb44b
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-09-20 23:00:36 +0900

    add songs

--- src/data/tracks.js
+++ src/data/tracks.js
@@ -1,5 +1,12 @@
+import abbyRoad from './img_cover_beatles_abby_road.png';
+import revolver from './img_cover_beatles_revolver.png';
+import rubberSoul from './img_cover_beatles_rubber_soul.png';
+import ForNoOne from  './for-no-one.m4a';
+import NorwegianWood from './norwegian-wood.m4a';
+import OhDarling from  './oh-darling.m4a';
+import Something from  './something.m4a';
+import TomorrowNeverKnows from './tomorrow-never-knows.m4a';
 import YouWontSeeMe from './you-wont-see-me.mp3';
-import Something from './Something.m4a';
 import trinix from './profile_image_Apple.jpg';
 
 export const tracks = [
@@ -7,12 +14,36 @@ export const tracks = [
     title: 'You Want See Me',
     src: YouWontSeeMe,
     author: 'The Beatles',
-    thumbnail: trinix,
+    thumbnail: rubberSoul,
+  },
+  {
+    title: 'Norwegian Wood',
+    src: NorwegianWood,
+    author: 'The Beatles',
+    thumbnail: rubberSoul,
+  },
+  {
+    title: 'Tomorrow Never Knows',
+    src: TomorrowNeverKnows,
+    author: 'The Beatles',
+    thumbnail: revolver,
+  },
+  {
+    title: 'For No One',
+    src: ForNoOne,
+    author: 'The Beatles',
+    thumbnail: revolver,
   },
   {
     title: 'Something',
     src: Something,
     author: 'The Beatles',
-    thumbnail: trinix,
-  }
+    thumbnail: abbyRoad,
+  },
+  {
+    title: 'Oh! Darling',
+    src: OhDarling,
+    author: 'The Beatles',
+    thumbnail: abbyRoad,
+  },
 ];
