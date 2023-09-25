e878700a6b62d6400d6fe12e2784ec90b6a9404a
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-09-25 15:59:13 +0900

    set up array object, shuffle and add calc

--- src/components/ContactParts.js
+++ src/components/ContactParts.js
@@ -26,8 +26,37 @@ const ContactParts = () => {
     setInput({name: "", age: 0});
   };
 
+  // 配列　シャッフルしてみる。
+  // 1から5までの配列を作る。
+  // `undefined`が入ってくる、無視したいので`_`を入れておく
+  const arr = [...Array(5)].map((_, idx) => idx + 1);
+  const [shaffleArr, setShuffleArr] = useState(arr);
+  // 配列の操作は別名を作ってから。
+  const clickBtn = () => {
+    const asSaveAsArr = [...shaffleArr];
+    const popedVal = asSaveAsArr.pop();
+    asSaveAsArr.unshift(popedVal);
+    setShuffleArr(asSaveAsArr);
+  }
+  
+  // 配列の中の値を合計してみる。
+  const addCalcArr = [...Array(5)].map((_, idx) => idx + 1);
+  const [addArr, setAddArr] = useState(addCalcArr);
+  const addCalc = () => {
+    if (typeof addArr === "object") {
+      const asSaveAddArr = [...addArr];
+      const result = asSaveAddArr.reduce((acc, n) => acc + n, 0);
+      return setAddArr(result);
+    }
+  }
+
+  const joinArrStr = (arr) => {
+    return arr.join(", ");
+  }
+
   return (
     <>
+      {/* カウンター */}
       <div className="wrapper">
         <p>カウント数／{count}</p>
         <button onClick={countUp}>Count Up</button>
@@ -35,7 +64,8 @@ const ContactParts = () => {
         <button onClick={resetCount}>Reset Count</button>
       </div>
 
-      {/* 問題点がある。
+      {/* input要素へ入力 */}
+      {/* 問題点がある。解決法見つけられてない。
       value={input.name}でリセットボタンは効くが、placeholderだとタメだ。
       ただ、ユーザビリティーを考えたらここはvalueではダメ。 */}
       <div className="wrapper input-sample">
@@ -54,6 +84,18 @@ const ContactParts = () => {
             <button onClick={resetInput}>Input Reset</button>
           </li>
         </ul>
+      </div>
+
+      {/* 配列 　シャッフルしてみる */}
+      <div className="wrapper for-arr">
+        <p>{shaffleArr}</p>
+        <button onClick={clickBtn}>シャッフルする</button>
+      </div>
+
+      {/* 合計してみる */}
+      <div className="wrapper for-arr">
+        <p>{addArr}</p>
+        <button onClick={addCalc}>合計する</button>
       </div>
     </>
   );
